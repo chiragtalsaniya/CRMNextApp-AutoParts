ae3ab65e366c81d92f4151f51a08e036acf33887
Chirag Talsaniya <30615271+chiragtalsaniya@users.noreply.github.com>
2025-06-25 18:19:03 -0400

    Enable HTTPS for Vite development server

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: false,
+    https: true,
     host: 'localhost',
     port: 5173,
     strictPort: true,
