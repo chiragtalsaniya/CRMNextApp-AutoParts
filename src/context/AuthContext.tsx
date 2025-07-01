e783f688f1e1e67502c5c37fb34137ce173d0b36
Chirag Talsaniya <chirag.talsaniya20@gmail.com>
2025-07-01 14:28:10 -0400

    Update AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useEffect, useContext } from 'react';
 import { User, UserRole } from '../types';
 import { authAPI, companiesAPI, storesAPI, retailersAPI } from '../services/api';
 
